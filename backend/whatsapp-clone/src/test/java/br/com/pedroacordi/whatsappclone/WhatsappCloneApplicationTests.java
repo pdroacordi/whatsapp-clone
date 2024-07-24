304da3a3c83a67d27a5d878d652a331a36391a49
Pedro Acordi <pedroacordi2@gmail.com>
2024-07-24 11:00:59 -0300

    project setup

---
+++ backend/whatsapp-clone/src/test/java/br/com/pedroacordi/whatsappclone/WhatsappCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroacordi.whatsappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
